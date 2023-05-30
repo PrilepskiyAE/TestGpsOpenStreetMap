886ac69bf0045012ccc0139eefb5c59406aee8ad
Prilepskiy_ae <alexey.prilepskiy@btb.dev>
2023-05-30 23:22:53 +0700

    add fragment

--- app/src/main/java/com/prilepskiy/trenninggpsopenstreetmap/MainActivity.kt
+++ app/src/main/java/com/prilepskiy/trenninggpsopenstreetmap/MainActivity.kt
@@ -2,10 +2,26 @@ package com.prilepskiy.trenninggpsopenstreetmap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.prilepskiy.trenninggpsopenstreetmap.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        onBottomNavClicks()
+    }
+    private fun onBottomNavClicks(){
+binding.bNav.setOnItemSelectedListener {
+    when(it.itemId){
+        R.id.id_home->{
+            Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show()}
+        R.id.id_tracks->{Toast.makeText(this, "tracks", Toast.LENGTH_SHORT).show()}
+        R.id.id_settings->{Toast.makeText(this, "settings", Toast.LENGTH_SHORT).show()}
+    }
+    true
+}
     }
 }
---
+++ app/src/main/java/com/prilepskiy/trenninggpsopenstreetmap/fragment/SettingsFragment.kt
@@ -0,0 +1,29 @@
+package com.prilepskiy.trenninggpsopenstreetmap.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.prilepskiy.trenninggpsopenstreetmap.R
+import com.prilepskiy.trenninggpsopenstreetmap.databinding.ActivityMainBinding
+import com.prilepskiy.trenninggpsopenstreetmap.databinding.FragmentMainBinding
+import com.prilepskiy.trenninggpsopenstreetmap.databinding.FragmentSettingsBinding
+
+
+class SettingsFragment : Fragment() {
+    lateinit var binding: FragmentSettingsBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = FragmentSettingsBinding.inflate(inflater)
+            return binding.root
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) = SettingsFragment()}
+}
